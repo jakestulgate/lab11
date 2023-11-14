dfe30d90bd2fb52439ecd305440cb2a966779f09
Jake Mac <jakestulgate@gmail.com>
2023-11-14 02:44:32 -0500

    t6

--- top.sv
+++ top.sv
@@ -133,12 +133,11 @@ module ll_alu #(
   
   always_comb begin
     // Adjust new altitude
-    alt_n = (alt_t <= 0) ? 0 : alt_t; // alt_t = alt - vel -> alt >= vel for alt > 0
-    vel_n = 0;
+    alt_n = (alt_t <= 0) ? 0 : alt_t;
     // Adjust new velocity
-    vel_n = (vel_n == 0) ? (fuel == 0) ? 0 : vel_t1 : vel_t2;
+    vel_n = (alt_t <= 0) ? 0 : ((fuel == 0) ? vel_t1 : vel_t2); // if new alt is <= 0 -> 0 otherwise use calculated
     // Adjust new fuel
-    fuel_n = (fuel_t < 0) ? 0 : fuel_t; // fuel_t = fuel - thrust -> fuel >= thrust for fuel > 0 
+    fuel_n = (fuel_t <= 0) ? 0 : fuel_t;
   end
 
 endmodule
