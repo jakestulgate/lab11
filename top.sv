27b926141238f5cb53d9d66ae79fd178d730ba1b
Jake Mac <jakestulgate@gmail.com>
2023-11-14 02:51:32 -0500

    0 to >= 0x5000

--- top.sv
+++ top.sv
@@ -133,11 +133,13 @@ module ll_alu #(
   
   always_comb begin
     // Adjust new altitude
-    alt_n = (alt_t <= 0) ? 0 : alt_t;
+    // alt_n = (alt_t <= 0) ? 0 : alt_t;
+    alt_n = (alt_t >= 0x5000) ? 0 : alt_t;
     // Adjust new velocity
-    vel_n = (alt_t <= 0) ? 0 : ((fuel == 0) ? vel_t1 : vel_t2); // if new alt is <= 0 -> 0 otherwise use calculated
+    vel_n = (alt_t >= 0x5000) ? 0 : ((fuel == 0) ? vel_t1 : vel_t2); // if new alt is <= 0 -> 0 otherwise use calculated
     // Adjust new fuel
-    fuel_n = (fuel_t <= 0) ? 0 : fuel_t;
+    // fuel_n = (fuel_t <= 0) ? 0 : fuel_t;
+    fuel_n = (fuel_t >= 0x5000) ? 0 : fuel_t;
   end
 
 endmodule
